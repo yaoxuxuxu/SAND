03e276a8ce9601c5ba532e5907243dc40d914164
Yao_xi <63792585+yaoxuxuxu@users.noreply.github.com>
2026-05-14 12:33:06 +0900

    start to do build in library

---
+++ SAND/library.py
@@ -0,0 +1,11 @@
+import importlib
+from .environment import Environment,TreeEnv
+class BuiltInLibraryManager:
+    def __init__(self):
+        self.libs_dir=""
+        self.libs=[]
+        self.generate_list()
+    def generate_list(self):
+        self.libs
+    def get_lib(self):
+        return self.env
