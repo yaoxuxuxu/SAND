3346db437a53af583e9f0894bbc771dad11981f9
Yao_xi <63792585+yaoxuxuxu@users.noreply.github.com>
2026-05-23 16:21:02 +0900

    fix and refactor eval primary && add eval factor

--- SAND/interpreter.py
+++ SAND/interpreter.py
@@ -34,6 +34,8 @@ class Interpreter:
             return self.do_state(astTree)
         if astTree.getType()=="expression":
             return self.do_expr(astTree)
+        if astTree.getType()=="factor":
+            return self.do_factor(astTree)
         if astTree.getType()=="primary":
             return self.do_pri(astTree)
         if astTree.getType()=="primary_array":
@@ -181,10 +183,13 @@ class Interpreter:
         num_state=len(astTree.child)
         if num_state!=2:
             raise InterpreterException("Bad while block!",astTree.getToken())
-        tmp=0
         while self.eval(astTree.child[0]):
-            tmp=self.eval(astTree.child[1])
-        return tmp
+            self.eval(astTree.child[1])
+        return None
+    def do_factor(self,astTree):
+        op=astTree.getValue()
+        if op=="-":
+            return -self.eval(astTree.getChild(0))
     def do_expr(self,astTree):
         if type(astTree) is ASTfruit and len(astTree.child)==2:
             op=astTree.getValue()
@@ -252,41 +257,48 @@ class Interpreter:
                 return token.getValue()
         return 0
     def do_pri(self,astTree,isLeft=False):
-        num_child=astTree.getChildNum()
-        child=astTree.getChild()
-        if num_child==0 and type(astTree)==ASTfruit:
-            varname=self.getVarNameFromAsttree(astTree)
-            if varname:
-                #special feature for return argument
-                if varname == "return_all":
-                    return ClassInstance(self.now_env)
-                #special feature ends
-                return self.now_env.getValue(varname)
-            return astTree.getValue()
-        elif astTree.getValue()=="lambda":
+        if astTree.getValue()=="lambda":
             return self.do_lambda(astTree)
-        elif num_child>0:
-            primary=astTree.getValue()
-            cnt=0
-            env=self.now_env
-            for postfix in child:
-                if postfix.getType()=="postfix":
-                    primary=self.do_postfix(primary,postfix,env)
-                elif postfix.getType()=="dot":
-                    env,primary=self.do_dot(primary,postfix,env)
-                    if isLeft and cnt==len(child)-1:
-                        return ("class",env,self.getVarNameFromAsttree(postfix.getChild(0)))
-                elif postfix.getType()=="array":
-                    if isLeft and cnt==len(child)-1:
-                        if isinstance(primary,str):
-                            primary=env.getValue(primary)
-                        return ("array",primary,self.eval(postfix.getChild(0)))
-                    primary=self.do_array(primary,postfix,env)
-                else:
-                    return self.eval(postfix)
-                cnt+=1
-            return primary
-        return self.eval(astTree.child[0])
+        else:
+            return self.seperate_pri(astTree,isLeft)
+    def seperate_pri(self,astTree,isLeft=False):
+        token=astTree.getToken()
+        if token==None:
+            #expression like (a+b).something or (a+b)[something]
+            if not astTree.hasChild():
+                return None
+            primary=self.eval(astTree.getChild(0))
+            return self.do_decorate_var(primary,astTree.getChild(),isLeft)
+        elif token.getType()=="NUMBER":
+            return token.getValue()
+        elif token.getType()=="STRING":
+            return token.getValue()
+        elif token.getType()=="IDENTIFIER":
+            varname=self.getVarNameFromAsttree(astTree)
+            if varname=="return_all":
+                print("123456!!")
+                return ClassInstance(self.now_env)
+            primary=self.now_env.getValue(varname)
+            return self.do_decorate_var(primary,astTree.getChild(),isLeft)
+        else:
+            raise InterpreterException("Unknown primary!",token)
+    def do_decorate_var(self,primary,child,isLeft=False):
+        
+        env=self.now_env
+        cnt=0
+        for postfix in child:
+            if postfix.getType()=="postfix":
+                primary=self.do_postfix(primary,postfix,env)
+            elif postfix.getType()=="dot":
+                env,primary=self.do_dot(primary,postfix,env)
+                if isLeft and cnt==len(child)-1:
+                    return ("class",env,self.getVarNameFromAsttree(postfix.getChild(0)))
+            elif postfix.getType()=="array":
+                if isLeft and cnt==len(child)-1:
+                    return ("array",primary,self.eval(postfix.getChild(0)))
+                primary=self.do_array(primary,postfix,env)
+            cnt+=1
+        return primary
     def create_classinstance(self,obj):
         instance=ClassInstance(obj.getEnv())
         self.eval_with_env(obj.getAsttree(),instance.getEnv())
@@ -313,26 +325,26 @@ class Interpreter:
         return primary.getValueByIndex(index)
         
     def do_dot(self,primary,postfix,env):
-        varname=postfix.getChild(0)
-        varname=self.getVarNameFromAsttree(varname)
-        
-        obj=env.getValue(primary)
-        #<obj>.<varname>
-        if isinstance(obj,BuiltInLibrary):
-            return env,obj.importFunByName(varname)
-        if isinstance(obj,Vector):
-            return env,obj.fetch_method(varname)
-        #a.b.c.d
-        if varname=="new":
-            if not isinstance(obj,Class):
+        nextpri=postfix.getChild(0)
+        nextpri=self.getVarNameFromAsttree(nextpri)
+        #<primary>.<nextpri>
+        #special case
+        if isinstance(primary,BuiltInLibrary):
+            return env,primary.importFunByName(nextpri)
+        if isinstance(primary,Vector):
+            return env,primary.fetch_method(nextpri)
+        #example:a.b.c.d
+        if nextpri=="new":
+            if not isinstance(primary,Class):
                 raise InterpreterException("Only class can create instance")
-            instance=self.create_classinstance(obj)
-            return obj.getEnv(),instance
+            instance=self.create_classinstance(primary)
+            return primary.getEnv(),instance
         else:
-            if not isinstance(obj,ClassInstance):
+            if not isinstance(primary,ClassInstance):
+                print(primary,nextpri,env.var)
                 raise InterpreterException("You should make a instance from class before to use")
-            env=obj.getEnv()
-            primary=env.getValue(varname)
+            env=primary.getEnv()
+            primary=env.getValue(nextpri)
             if isinstance(primary,Function):
                 primary=primary.copy()
                 primary.setEnv(env)
@@ -346,12 +358,10 @@ class Interpreter:
         return Function("lambda",args,fun,self.now_env)
     def do_postfix(self,primary,postfix,env):
         args=self.get_args(postfix)
-        fun_name=primary
-        result=self.do_fun(fun_name,args)
+        fun=primary
+        result=self.do_fun(fun,args)
         return result
     def do_fun(self,fun,args):
-        if isinstance(fun,str):
-            fun:Function=self.now_env.getValue(fun)
         if isinstance(fun,NativeFunction):
             fun.setArgs(args)
             return self.nfm.eval(fun)
