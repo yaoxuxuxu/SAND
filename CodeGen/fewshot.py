d376ec991b50c3c869bff34d81a01f72809d59b5
Yao_xi <63792585+yaoxuxuxu@users.noreply.github.com>
2026-06-15 13:51:11 +0900

    make main sand only

--- CodeGen/fewshot.py
+++
@@ -1,9 +0,0 @@
-from .modelManager import ModelManager
-class baseline(ModelManager):
-    def __init__(self):
-        super().__init__()
-        self.user_add("""you should only use the programming language sand.
-                      sand is a self made script programming language and it is similar to 
-                      the programming language STONE.Stone is a self made PL in a PL book.
-                      its writter is Professor Chiba from Tokyo University, Chiba Software Lab.""")
-    
--- CodeTest/test_template.py
+++
@@ -1,7 +0,0 @@
-def one_function_testcode(param):
-    code=f"""
-    import testcode
-    import stdcode
-    testcode.function({param})==stdcode.function({param})
-    """
-    return code
