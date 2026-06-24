47a9a3549a98f5c47e596e6f996e9cfe49ed3907
Yao_xi <63792585+yaoxuxuxu@users.noreply.github.com>
2026-06-24 14:17:02 +0900

    add apipool remove modelpool

--- CodeGen/modelManager.py
+++ CodeGen/modelManager.py
@@ -2,16 +2,17 @@ from google import genai
 from google.genai import types
 import time
 class ModelManager:
-    def __init__(self,model=None):
-        self.client = genai.Client(api_key=self.readApiKey())
+    def __init__(self,model):
+        self.api_pool=self.readApiKey()
+        self.api_index=-1
+        self.change_api()
         #self.show_available_models()
-        self.model_pool=0
-        if model:
-            self.useful_models=[model]
-        else:
-            self.useful_models=["gemini-2.5-flash","gemini-3-flash-preview","gemma-4-26b-a4b-it","gemma-4-31b-it"]
+        self.model_name=model
         self.history=[]
         #self.chat=self.client.chats.create(model="gemini-2.5-flash",config=self.config())
+    def change_api(self):
+        self.api_index=(self.api_index+1)%len(self.api_pool)
+        self.client = genai.Client(api_key=self.api_pool[self.api_index])
     def show_available_models(self):
         models = self.client.models.list()
         for model in models:
@@ -19,8 +20,15 @@ class ModelManager:
         return
     def readApiKey(self):
         with open("./CodeGen/apikey","r+") as fp:
-            api=fp.read()
-        return api
+            res=fp.read()
+        apis=[]
+        for i in res.split("\n"):
+            if len(i)<5:
+                continue
+            if "#" in i:
+                continue
+            apis.append(i)
+        return apis
     def config(self,config_prompt):
         return {"system_instruction": config_prompt}
     def user_add(self,message):
@@ -57,14 +65,15 @@ class ModelManager:
             contexts = self.history
         while True:
             try:
-                res=self.client.models.generate_content(model=self.useful_models[self.model_pool],
+                res=self.client.models.generate_content(model=self.model_name,
                                                     config=self.config(config_prompt),
                                                     contents=contexts)
                 break
             except Exception as e:
                 print(f"Error occurred: {e}")
+                if "exceed" in str(e):
+                    self.change_api()
                 time.sleep(5)  # Wait for 1 second before retrying
-                self.model_pool=(self.model_pool+1)%len(self.useful_models)
         self.assistant_add(res.text)
         return res.text
 if __name__ == "__main__":
