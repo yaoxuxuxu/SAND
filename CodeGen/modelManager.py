f2177dc540d845ad7273b1513045e7c1f30ee866
Yao_xi <63792585+yaoxuxuxu@users.noreply.github.com>
2026-07-13 18:32:33 +0900

    creating interface for groq

--- CodeGen/modelManager.py
+++ CodeGen/modelManager.py
@@ -1,7 +1,6 @@
 import os
-from google import genai
-from google.genai import types
 import time
+from groq import Groq
 class ModelManager:
     def __init__(self,model):
         self.api_pool=self.readApiKey()
@@ -13,16 +12,16 @@ class ModelManager:
         #self.chat=self.client.chats.create(model="gemini-2.5-flash",config=self.config())
     def change_api(self):
         self.api_index=(self.api_index+1)%len(self.api_pool)
-        self.client = genai.Client(api_key=self.api_pool[self.api_index])
+        self.client = Groq(api_key=self.api_pool[self.api_index])
     def show_available_models(self):
         models = self.client.models.list()
-        for model in models:
-            print(model.name)
+        for model in models.data:
+            print(model.id)
         return
     def readApiKey(self):
         while True:
             try:
-                with open("./CodeGen/apikey","r+") as fp:
+                with open("./CodeGen/groq_apikey","r+") as fp:
                     res=fp.read()
                 break
             except Exception as e:
@@ -38,60 +37,54 @@ class ModelManager:
             apis.append(i)
         return apis
     def config(self,config_prompt):
-        return {"system_instruction": config_prompt}
+        return {"role": "system","content": config_prompt}
     def user_add(self,message):
-        message=types.UserContent(
-        parts=[types.Part(text=message)]
-        )
+        message={
+        "role": "user",
+        "content": message
+        }
         self.history.append(message)
     def assistant_add(self,message):
-        message=types.Content(
-        parts=[types.Part(text=message)]
-        )
+        message={
+        "role": "assistant",
+        "content": message
+        }
         self.history.append(message)
-    def getLastSolution(self,returnText=True):
-        cnt=len(self.history)-1
-        while cnt>=0:
-            text=self.history[cnt].parts[0].text
-            if text == "!!!next stage!!!" and len(text)<20:
-                cnt-=1
-                continue
-            if type(self.history[cnt])==types.Content:
-                if returnText:
-                    return text
-                return self.history[cnt]
-            cnt-=1
-        return None
-    
-    def user_input(self):
-        res=input()
-        self.user_add(res)
-        return
     
     def send(self,config_prompt,contexts=None):
+        #context change
         if contexts is None:
             contexts = self.history
+        #system prompt setting
+        if contexts[0]["role"]!="system":
+            contexts.insert(0, self.config(config_prompt))
+        else:
+            contexts[0]["content"]=config_prompt
+        
         while True:
             try:
-                res=self.client.models.generate_content(model=self.model_name,
-                                                    config=self.config(config_prompt),
-                                                    contents=contexts)
-                assert(res.text!=None)
+                res = self.client.chat.completions.create(
+                    model=self.model_name,
+                    messages=contexts
+                )
+                assert(res!=None)
                 break
             except Exception as e:
                 print(f"Error occurred: {e}")
-                if "exceed" in str(e):
-                    self.change_api()
                 time.sleep(5)  # Wait for 1 second before retrying
-        self.assistant_add(res.text)
-        return res.text
+        res=res.choices[0].message.content
+        self.assistant_add(res)
+        return res
+    
 if __name__ == "__main__":
-    mm=ModelManager()
+    mm=ModelManager("llama-3.1-8b-instant")
+    #mm.show_available_models()
     while True:
         s=input()
         if s=="exit":
             break
-        res=mm.send(s)
+        mm.user_add(s)
+        res=mm.send("")
         print(res)
     print(mm.history)
     
