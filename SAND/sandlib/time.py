c1064fec463c0150e57f6e0b8794abe5b9ae833d
Yao_xi <63792585+yaoxuxuxu@users.noreply.github.com>
2026-08-31 16:51:19 +0900

    add date_stamp in Time lib

--- SAND/sandlib/time.py
+++ SAND/sandlib/time.py
@@ -6,3 +6,14 @@ class Time(StandardLibrary):
         
     def time(self):
         return time.time()
+    def date_stamp(self):
+        date=""
+        lt=time.localtime()
+        date+=str(lt.tm_year)+"-"
+        date+=str(lt.tm_mon)+"-"
+        date+=str(lt.tm_mday)+" "
+
+        date+=str(lt.tm_hour)+":"
+        date+=str(lt.tm_min)+":"
+        date+=str(lt.tm_sec)
+        return date
